e8e813533c41d3ca42b51976bb2f983211862db6
vverheyen <90403002+vverheyen@users.noreply.github.com>
2022-02-07 09:51:37 -0600

    Update CardGameClient.java

--- src/CardGameClient.java
+++ src/CardGameClient.java
@@ -1,5 +1,7 @@
 public class CardGameClient {
     public static void main(String[] args) throws Exception {
         
+        Deck deck1 = new Deck();
+        System.out.println(deck1);
     }
 }
