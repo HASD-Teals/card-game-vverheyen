a804734d4cb808011a338888a5a0901b3837c062
vverheyen <90403002+vverheyen@users.noreply.github.com>
2022-02-11 08:54:36 -0600

    Update CardGameClient.java

--- src/CardGameClient.java
+++ src/CardGameClient.java
@@ -1,7 +1,29 @@
 public class CardGameClient {
     public static void main(String[] args) throws Exception {
+        //create two decks
+        //shuffle decks
+        //for loop through both and report each match
+        //then report total number of matches at end of loop
         
         Deck deck1 = new Deck();
-        System.out.println(deck1);
+        Deck deck2 = new Deck();
+        
+        deck1.shuffleCards();
+        deck2.shuffleCards();
+
+        comparing(deck1, deck2);
+
+    }
+    public static void comparing(Deck deck, Deck deck2) {
+        int counter=0;
+        for (int i=0; i<52; i++) {
+            if (deck[i]==deck2[i]) {
+                System.out.println("There is a match at "+i);
+                counter++;
+            }
+            if (counter==0) {
+                System.out.println("There are no matches");
+            }
+        }
     }
 }
